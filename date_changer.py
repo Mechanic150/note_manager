65f882db615f82253ebcc35450d92fce68e2ffcf
Mechanic150 <kirill-r-st@yandex.ru>
2024-12-21 14:48:36 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,4 @@
+created_date = '21.12.2024'
+issue_date = '31.12.2024'
+print(created_date[:5])
+print(issue_date[:5])
---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Кирилл'
+title = 'Новая заметка'
+content = 'Моя заметка'
+status = 'В работе'
+created_date = '20-12-2024'
+issue_date = '31-12-2024'
+print('Имя пользователя: ', username)
+print('Заголовок заметки: ', title)
+print('Описание заметки: ', content)
+print('Статус заметки: ', status)
+print('Дата создания заметки: ', created_date)
+print('Дата истечения заметки: ', issue_date)
