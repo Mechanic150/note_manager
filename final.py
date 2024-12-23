84cf23af1b5c88d75c9791dbce4c259bf0844749
Mechanic150 <kirill-r-st@yandex.ru>
2024-12-23 16:18:16 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,18 @@
+username = input('Ваше имя: ')
+title = input('Название заметки: ')
+title1 = input('Заголовок 1: ')
+title2 = input('Заголовок 2: ')
+content = input('Описание заметки: ')
+status = input('Статус заметки: ')
+created_date = input('Дата создания заметки в формате ДД.ММ.ГГГГ: ')
+issue_date = input('Дата истечения заметки в формате ДД.ММ.ГГГГ: ')
+titles = [title, title1, title2]
+note = [
+    username,
+    titles,
+    content,
+    status,
+    created_date,
+    issue_date
+]
+print(note)
