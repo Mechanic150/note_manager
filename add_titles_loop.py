58393cb93b9bafc37c2b1eb6eb5d95aa5c827764
Mechanic150 <kirill-r-st@yandex.ru>
2025-01-15 17:41:08 +0300

    Add files via upload

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -1,23 +1,17 @@
+titles = []
 while True:
     title = input('Введите название заметки: ')
     if title == '':
-        print('Заголовок нужно ввести обязательно!')
+        print('Название нужно ввести обязательно!')
     else: break
-title_1 = input('Введите заголовок 1 или нажмите Enter, чтобы продолжить: ')
-title_2 = input('Введите заголовок 2 или нажмите Enter, чтобы продолжить: ')
-if title_1 == '' and title_2 == '':
-    print('Название заметки: ', title)
-    print('Заголовков нет')
-if title_2 == '' and title_1 != '':
-    print('Название заметки: ', title)
-    print('Заголовки заметки: ')
-    print('- ', title_1)
-if title_1 != '' and title_2 != '':
-    print('Название заметки: ', title)
-    print('Заголовки заметки: ')
-    print('- ', title_1)
-    print('- ', title_2)
-if title_1 == '' and title_2 != '':
-    print('Название заметки: ', title)
-    print('Заголовки заметки: ')
-    print('- ', title_2)
+while True:
+    answ = input('Нажмите Enter, чтобы добавить еще заголовок или введите Стоп для завершения: ')
+    if answ == '':
+        title_1 = input('Введите название заголовка: ')
+        if title_1 == '':
+            print('Заголовок нужно ввести обязательно!')
+        if title_1 != '':
+            titles.append(title_1)
+    if answ == 'Стоп':
+        break
+print('Название заметки: ', title, '\n', 'Заголовки заметки: ', titles)
